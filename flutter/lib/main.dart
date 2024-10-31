4c5f98c5bf7367a7e2bee95f14ab61cf456852ff
wondongjin <82082770+dongjin97@users.noreply.github.com>
2024-10-31 20:12:59 +0900

    DS_Store 삭제

--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -23,7 +23,7 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Home'),),
+      appBar: AppBar(title: Text('Main'),),
     );
   }
 }
