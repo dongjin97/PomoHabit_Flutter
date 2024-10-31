60adf88872d91f09dc9bd6974b7bd4a1e3c0fbfc
wondongjin <82082770+dongjin97@users.noreply.github.com>
2024-10-31 20:10:18 +0900

    init test & add gitignore

--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -22,6 +22,8 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(title: Text('Home'),),
+    );
   }
 }
